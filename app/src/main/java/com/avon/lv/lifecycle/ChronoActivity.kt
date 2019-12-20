28ac441d51c36f1b6ee43080418c845e2a9a950f
SeungHyunBaek <back947@naver.com>
2019-12-20 21:46:11 +0900

    Study lifecycle

--- app/src/main/java/com/avon/lv/lifecycle/ChronoActivity.kt
+++ app/src/main/java/com/avon/lv/lifecycle/ChronoActivity.kt
@@ -1,28 +1,56 @@
 package com.avon.lv.lifecycle
 
 import android.os.Bundle
-import android.widget.Chronometer
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.avon.lv.R
 
 class ChronoActivity : AppCompatActivity() {
+
+    private val mLiveDataTimerViewModel: LiveDataTimerViewModel by lazy {
+        ViewModelProvider(this).get(LiveDataTimerViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chrono)
 
-//        val chronometerViewModel = ViewModelProvider(this).get(ChronometerViewModel::class.java)
+        // Chronometer 3
+        subscribe()
 
+
+        /* Chronometer 2
+        val chronometerViewModel = ViewModelProvider(this).get(ChronometerViewModel::class.java)
         val chronometer = findViewById<Chronometer>(R.id.chronometer)
 
-//        if (chronometerViewModel.getStartTime() == null) {
-//            val startTime: Long = SystemClock.elapsedRealtime()
-//            chronometerViewModel.setStartTime(startTime)
-//            chronometer.base = startTime
-//        } else {
-//            chronometer.base = chronometerViewModel.getStartTime()!!
-//        }
+        if (chronometerViewModel.getStartTime() == null) {
+            val startTime: Long = SystemClock.elapsedRealtime()
+            chronometerViewModel.setStartTime(startTime)
+            chronometer.base = startTime
+        } else {
+            chronometer.base = chronometerViewModel.getStartTime()!!
+        }
 
         chronometer.start()
+        */
+    }
+
+    fun subscribe() {
+        val elapsedTimeObserver: Observer<Long> by lazy {
+            object : Observer<Long> {
+                override fun onChanged(aLong: Long?) {
+                    var newText: String =
+                        this@ChronoActivity.resources.getString(R.string.seconds, aLong)
+                    findViewById<TextView>(R.id.timer_textview).setText(newText)
+                    Log.d("ChronoActivity3", "UpdatingTimer")
+                }
+            }
+        }
+
+        //TODO: observe the ViewModel's elapsed time
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
--- app/src/main/java/com/avon/lv/lifecycle/LiveDataTimerViewModel.kt
+++ app/src/main/java/com/avon/lv/lifecycle/LiveDataTimerViewModel.kt
@@ -1,23 +1,46 @@
 package com.avon.lv.lifecycle
 
 import android.os.SystemClock
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
+
 class LiveDataTimerViewModel : ViewModel() {
+
+    // 사용자 지정 크로노미터로 변경
+    // 타이머를 사용하여 1초마다 UI변경
+    // ViewModel에서 UI를 직접 수정하지 않고 데이터 원본을 관찰하고 있다가 데이터가 변경될 때 수신받음
+
     companion object {
-        val ONE_SECOND: Int = 1000
+        val ONE_SECOND: Long = 1000
     }
 
-    private val mElasedTime: MutableLiveData<Long> = MutableLiveData<Long>()
+    private val mElapsedTime: MutableLiveData<Long> = MutableLiveData<Long>()
 
-    private var mInitialTime:Long
-    private val timer:Timer
+    private var mInitialTime: Long
+    private val timer: Timer
 
     init {
         mInitialTime = SystemClock.elapsedRealtime()
         timer = Timer()
-        
+
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+
+                mElapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND, ONE_SECOND)
+    }
+
+    fun getElapsedTime(): LiveData<Long> {
+        return mElapsedTime
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
     }
 }
