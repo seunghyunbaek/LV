02821e613aa6f64592078d6243376892ab227027
SeungHyunBaek <back947@naver.com>
2020-04-13 23:52:38 +0900

    Create BmiCalculation for Test

---
+++ app/src/main/java/com/avon/lv/Chapter7BMICalculation/BmiActivity.kt
@@ -0,0 +1,13 @@
+package com.avon.lv.Chapter7BMICalculation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avon.lv.R
+
+class BmiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi)
+    }
+}
